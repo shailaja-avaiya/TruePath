a49f3cea5f94b601424b55c0297db236e9eb041c
shailaja-avaiya <shailaja.avaiya@gmail.com>
2026-03-19 16:20:26 +0530

    About us image change final

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaTools, FaHandshake, FaLightbulb, FaTrophy, FaUsers, FaIndustry, FaBuilding } from 'react-icons/fa';
 import './AboutPage.css';
-import aboutusImage from '../assets/building.png'; // Add a placeholder image for the facility
+import aboutusImage from '../assets/TPSVbuilding.png'; // Add a placeholder image for the facility
 
 const AboutPage = () => {
   return (
